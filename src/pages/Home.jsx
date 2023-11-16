3ff2092a99b92ffbcce430e0eda852ed846ad4f4
Sweet-Zip <sk.kimkuong@gmail.com>
2023-11-17 02:38:03 +0700

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import NavBar from '../components/Navbar';
 
 export default function Home() {
   return (
-
     <div className='justify-between items-center h-24 max-w-[1280px] mx-auto px-4'>
       <nav>
         <NavBar />
